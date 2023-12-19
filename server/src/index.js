e8bb3e9a6886dcd6194121d1315b507724417d28
manu <manuelmonjes@gmail.com>
2023-12-19 11:47:49 -0300

    update middleware

--- server/src/index.js
+++ server/src/index.js
@@ -3,15 +3,14 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io'
 import morgan from 'morgan';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import router from '../route/route.js';
 
 const PORT = 3000 ?? process.env.PORT;
 const DB = 'ChatBD' ?? process.env.DB;
 const USER = 'manu' ?? process.env.USER;
 const PASS = '71UFkz7usEtnXQhp' ?? process.env.PASS;
+
 const URI = `mongodb+srv://${USER}:${PASS}@cluster0.ku5gd.mongodb.net/${DB}?retryWrites=true&w=majority`
 mongoose.Promise = global.Promise;
 
@@ -23,8 +22,8 @@ const io = new Server(server, {
 });
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
