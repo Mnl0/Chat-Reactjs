5ce6dcc4dc725325d441f0765e0022c9cc3e0281
manu <manuelmonjes@gmail.com>
2023-12-19 22:35:25 -0300

    maquetacion

--- front/src/App.js
+++ front/src/App.js
@@ -3,11 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <div className="main">
+
+        <div className="leftSide">
+          <h1>lista de amigos</h1>
+        </div>
+
+        <div className="listMessage">
+          <div className="list">
+            <p>Lista mensajes</p>
+            <p className="p1">Lista mensajes</p>
+            <p>Lista mensajes</p>
+          </div>
+        </div>
+
+        <div className="formMessage">
+          <input type="text" placeholder="mensaje"></input>
+          <input type="text" placeholder="nombre"></input>
+          <button>enviar</button>
+        </div>
+
+      </div>
     </div>
   );
 }
